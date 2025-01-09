2456fc483fe651fb451a119a5e6c12ea14107357
Dewzera <1305111706@qq.com>
2025-01-09 19:42:44 +0800

    feat(annotation): 新增单元格合并和格式化注解
    - 新增 CellMerge 注解，用于合并列相同项
    - 新增 ExcelDictFormat 注解，用于字典格式化
    - 新增 ExcelEnumFormat 注解，用于枚举格式化

---
+++ common/excel/src/main/java/excel/annotation/CellMerge.java
@@ -0,0 +1,24 @@
+package excel.annotation;
+
+import excel.core.CellMergeStrategy;
+
+import java.lang.annotation.*;
+
+/**
+ * excel 列单元格合并(合并列相同项)
+ * <p>
+ * 需搭配 {@link CellMergeStrategy} 策略使用
+ *
+ * @author hxy
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface CellMerge {
+
+    /**
+     * col index
+     */
+    int index() default -1;
+
+}
