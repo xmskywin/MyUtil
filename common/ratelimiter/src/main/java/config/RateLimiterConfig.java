c454dc8ab7d9c688d321dcc4cbb7d065a3f934f6
Dewzera <1305111706@qq.com>
2025-04-18 15:50:27 +0800

    feat(common): 添加自定义限流注解和配置
    - 新增 RateLimiter 注解，用于方法级别的限流
    - 添加 RateLimiterConfig 配置类，自动配置限流切面
    -支持 Redis 作为限流存储后端

---
+++ common/ratelimiter/src/main/java/config/RateLimiterConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import aspectj.RateLimiterAspect;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConfiguration;
+
+/**
+ * @author hxy
+ */
+@AutoConfiguration(after = RedisConfiguration.class)
+public class RateLimiterConfig {
+
+    @Bean
+    public RateLimiterAspect rateLimiterAspect() {
+        return new RateLimiterAspect();
+    }
+
+}
